3c9b6b4901f18c6c8bc8abef0c408e6881224169
Prince Narteh <prinarteh@gmail.com>
2024-02-08 04:09:38 +0000

    feat: add iconify

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import { Icon } from "@iconify/react";
 
 export const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="h-screen w-80 shadow-2xl">
+      <div className="flex justify-center items-center py-4 gap-2">
+        <Icon
+          icon="arcticons:expense-register"
+          fontSize={30}
+          className="text-white"
+        />
+        <h3 className="text-2xl font-bold tracking-wide">
+          <span className="text-brand-turquoise">Expense</span>
+          <span className="text-brand-orange">Tracker</span>
+        </h3>
+      </div>
+    </aside>
+  );
 };
